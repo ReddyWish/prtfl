739c2f8c607ecf4620cb3f80085616e1b3f99a2a
Ilya <ilya.shuvatov1@gmail.com>
2023-11-06 21:18:22 +0100

    fixed react-hook-form

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
 import styles from './style.module.scss';
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 function Footer(props) {
 
